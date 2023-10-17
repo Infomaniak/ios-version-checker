a7eef945ce4cdef0ef7ab5300d01cf992c9f9e90
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2023-10-17 09:35:37 +0200

    chore: Rename folders

---
+++ Tests/VersionCheckerTests/ios_version_checkerTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import ios_version_checker
+
+final class ios_version_checkerTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
