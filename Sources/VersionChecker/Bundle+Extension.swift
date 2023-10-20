70e86bfe3e0c7404ccbbd5269105c9eaaa448f02
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2023-10-20 13:06:22 +0200

    fix: IsRunningInTestflight internal

--- Sources/VersionChecker/Bundle+Extension.swift
+++ Sources/VersionChecker/Bundle+Extension.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public extension Bundle {
+extension Bundle {
     /// Indicates whether the app is currently running in a TestFlight environment.
     var isRunningInTestFlight: Bool {
         guard let path = appStoreReceiptURL?.path else {
