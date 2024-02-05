de8895839345d82a417b6b32b5f5840fd03f7c0f
Valentin Perignon <valentin.perignon@infomaniak.com>
2024-02-05 16:55:15 +0100

    feat: Add localization

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "VersionChecker",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
