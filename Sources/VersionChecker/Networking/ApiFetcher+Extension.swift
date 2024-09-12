3d3525187e3ad88d68a0ca6517050544e0290dd0
Matthieu Déglon <matthieu.deglon@infomaniak.com>
2024-09-12 14:45:31 +0200

    fix: Reduce API call

--- Sources/VersionChecker/Networking/ApiFetcher+Extension.swift
+++ Sources/VersionChecker/Networking/ApiFetcher+Extension.swift
@@ -22,7 +22,8 @@ import InfomaniakCore
 
 extension Endpoint {
     static func version(store: Store, platform: Platform, appName: String) -> Endpoint {
-        return Endpoint(path: "/1/app-information/versions/\(store.rawValue)/\(platform.rawValue)/\(appName)")
+        let fields = "id,min_version,published_versions.build_min_os_version"
+        return Endpoint(path: "/1/app-information/versions/\(store.rawValue)/\(platform.rawValue)/\(appName)?only=\(fields)")
     }
 }
 
