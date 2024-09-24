2d5410d290326117628e350fdf7dc1434350832b
Valentin Perignon <valentin.perignon@infomaniak.com>
2024-09-24 16:30:08 +0200

    fix: Property "published_versions.type" is missing

--- Sources/VersionChecker/Networking/ApiFetcher+Extension.swift
+++ Sources/VersionChecker/Networking/ApiFetcher+Extension.swift
@@ -25,7 +25,7 @@ extension Endpoint {
         return Endpoint(path: "/1/app-information/versions/\(store.rawValue)/\(platform.rawValue)/\(appName)", queryItems: [
             URLQueryItem(
                 name: "only",
-                value: "min_version,published_versions.tag,published_versions.tag_updated_at,published_versions.build_version,published_versions.build_min_os_version"
+                value: "min_version,published_versions.tag,published_versions.tag_updated_at,published_versions.type,published_versions.build_version,published_versions.build_min_os_version"
             )
         ])
     }
